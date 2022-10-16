ede9030a52615a295ed5712acb8777589db79d8f
CHJ30 <caihongjian98@gmail.com>
2022-10-16 23:32:07 +0800

    init

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
 const notifier = require("node-notifier");
 
-/// 有两种方法可以使用 `node-notifier` 创建和推送简单的通知。
-// 将一个字符串传递给 `notify()` 函数:
+const pluginName = "NotifierPlugin";
+class NotifierPlugin {
+  // 接受参数
+  constructor(option) {
+    this.option = option;
+    console.log(option);
+  }
+  apply(compiler) {
+    console.log("MyPlugin 启动");
+    // 调用emit勾子，挂载函数
+    // compilation => 此次打包的上下文
+    compiler.hooks.emit.tap("MyPlugin", (compilation) => {
+      // 遍历资源文件信息
+      // 键name为每个资源的名称
+      for (const name in compilation.assets) {
+        // console.log(name)
+        if (name.endsWith(".js")) {
+          // 每个资源的值
+          const contents = compilation.assets[name].source();
+          const withoutComments = contents.replace(/\/\*\*+\*\//g, "");
+          // 覆盖原始对象
+          compilation.assets[name] = {
+            source: () => withoutComments,
+            size: () => withoutComments.length,
+          };
+        }
+      }
+    });
+  }
 
-// 或者，也可以传递一个对象，并设置属性，如标题和消息:
-notifier.notify({
-  title: "爸爸",
-  message: "我构建好了",
-});
+  //   apply(compiler) {
+  //     // 注册函数
+  //     compiler.hooks.entryOption.tap(
+  //       "NotifierPlugin",
+  //       (compilation, callback) => {
+  //         // dosomething
+  //         //   notifier.notify({
+  //         //     title: option.title,
+  //         //     message: option.beforePackMessage,
+  //         //   });
+  //         callback();
+  //       }
+  //     );
+  //     // compiler.hooks.done.tap(pluginName, (compilation, callback) => {
+  //     //   // dosomething
+  //     //   notifier.notify({
+  //     //     title: option.title,
+  //     //     message: option.afterPackMessage,
+  //     //   });
+  //     //   callback();
+  //     // });
+  //   }
+}
+module.exports = NotifierPlugin;
